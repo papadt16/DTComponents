d5ba7abcd3c079036d5276f1c74122641f366ea0
papadt16 <149633903+papadt16@users.noreply.github.com>
2025-12-18 02:13:38 +0100

    Integrate react-toastify for notifications
    Added ToastContainer for notifications in the main app.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer position="top-right" autoClose={3000} />
     </BrowserRouter>
   </React.StrictMode>
 );
