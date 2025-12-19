57c42a73d0969de59d3a5a3573ba2455a7191e2d
papadt16 <149633903+papadt16@users.noreply.github.com>
2025-12-19 21:27:57 +0100

    Update ProductDetails.jsx

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "axios"; 
 
 const API = "https://dtcomponents-backend.onrender.com";
 
